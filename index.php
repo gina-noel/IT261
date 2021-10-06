1ffbcf2cd249f39747d1f194f16f753e24929969
gina-noel <Gina.Noel@seattlecolleges.edu>
2021-10-06 11:58:30 -0700

    testing git..

--- index.php
+++ index.php
@@ -4,11 +4,8 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 <link href="css/styles.css" type="text/css" rel="stylesheet">
-<<<<<<< HEAD
-<title>Gina's Portal Page</title>
-=======
-<title>My Portal Page</title>
->>>>>>> origin/master
+<title>Gina Noel's Portal Page</title>
+
 
 </head>
 
