253e0e2fefe75f2dffd13d6d9bd22bf58003d2a4
gina-noel <Gina.Noel@seattlecolleges.edu>
2021-10-06 11:41:39 -0700

    Testing git

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 <link href="css/styles.css" type="text/css" rel="stylesheet">
-<title>My Portal Page</title>
+<title>Gina's Portal Page</title>
 
 </head>
 
